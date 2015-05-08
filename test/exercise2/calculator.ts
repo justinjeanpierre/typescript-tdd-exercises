28b5e540ebcb0359853c4e8337304168d3783d63
justinjeanpierre <justin.jeanpierre@gmail.com>
2015-05-08 15:00:52 -0400

    Added test implementations.

--- test/exercise2/calculator.ts
+++ test/exercise2/calculator.ts
@@ -22,12 +22,21 @@ module Calculator {
                         assert.ok(expression.equals());
                     });
                     it('should return false if the two expression evaluations are not equal', () => {
+                        var equalExpression = new ExpressionComparer(new NumberExpression(5), new NumberExpression(4));
+                        assert.ok(!equalExpression.equals());
                     });
                 });
                 describe('greaterThan', () => {
                     it('should return true if the left expression is greater than the right', () => {
+                        var greaterThanExpression = new ExpressionComparer(new NumberExpression(7), new NumberExpression(3));
+                        assert.ok(greaterThanExpression.greaterThan());
                     });
                     it('should return false if the left expression is equal to or less than the right', () => {
+                        var lessThanExpression = new ExpressionComparer(new NumberExpression(1), new NumberExpression(6));
+                        assert.ok(!lessThanExpression.greaterThan());
+
+                        var equalExpression = new ExpressionComparer(new NumberExpression(6), new NumberExpression(6));
+                        assert.ok(!equalExpression.greaterThan());
                     });
                 });
             });
